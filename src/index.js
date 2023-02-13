04e59f7ff9c6bb731a473a4d0988ef05692fbd89
Galiia-GR <hope05590@gmail.com>
2023-02-13 19:39:11 +0600

    add result 50%

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const config = bracketsConfig + "";
+  const arrConfig = config.split(",");
+  const cofigOpen = ["(", "{", "[", "|"];
+  const configClose = [")", "}", "]", "|"];
+
+  let stack = [];
+
+  const newOpen = arrConfig.filter((el) => cofigOpen.includes(el));
+  const newClose = arrConfig.filter((el) => configClose.includes(el));
+
+  for (let i = 0; i < str.length; i++) {
+    let symbol = str[i];
+
+    if (newOpen.includes(symbol)) {
+      stack.push(symbol);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+
+      let highSymbol = stack[stack.length - 1];
+
+      if (newClose != highSymbol) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0 ? true : false;
+};
